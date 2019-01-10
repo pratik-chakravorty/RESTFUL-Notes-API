007a96a15442ab38f03daa6ac10d9b044841868c
Pratik <pchakravorty@Pratiks-MacBook-Pro.local>
2019-01-10 21:44:53 +0000

    Added some comments in app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+//gets stuff from the db
 const db = require('./db');
 
 const app = express();
 
+//follows the mvc pattern here.
 const NotesController = require('./notes/NotesController');
 
 
